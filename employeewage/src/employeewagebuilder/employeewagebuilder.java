e84bb9e51afd86d4b9a7992de9d47a28ad47839b
pavan sriram prasad <pittapavansriramprasad@gmailcom>
2021-05-07 10:23:29 +0530

    check employee status

--- employeewage/src/employeewagebuilder/employeewagebuilder.java
+++ employeewage/src/employeewagebuilder/employeewagebuilder.java
@@ -4,7 +4,13 @@ public class employeewagebuilder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
+			       // constants
+			       int IS_FULL_TIME = 1;
+			       // computation
+			       double empCheck = Math.floor(Math.random() * 10) % 2;
+			       if (empCheck == IS_FULL_TIME)
+			       System.out.println("Employe is Present");
+			      else
+			       System.out.println("Employe is Absent");
+			    }
 }
