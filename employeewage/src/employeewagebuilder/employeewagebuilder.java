8a7b2ed5881de7f93a20d61c7264278d52eac942
pavan sriram prasad <pittapavansriramprasad@gmailcom>
2021-05-07 10:49:12 +0530

    Calculating Wages for a month Assuming 20 Working days Per Month

--- employeewage/src/employeewagebuilder/employeewagebuilder.java
+++ employeewage/src/employeewagebuilder/employeewagebuilder.java
@@ -4,26 +4,33 @@ public class employeewagebuilder {
 	 public static final int IS_PART_TIME = 1;
 	   public static final int IS_FULL_TIME = 2;
 	   public static final int EMP_RATE_PER_HOUR = 20;
+	   public static final int  NO_OF_WORKING_DAYS = 20;
 	   
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			   //variables
+        	  //variables
 			   int empHrs = 0;
 			   int empWage = 0;
-			   // computation
-			  int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-			  switch (empCheck) {
+			   int totalEmpWage = 0;
+			 //computation
+			 for (int day = 0; day < NO_OF_WORKING_DAYS; day++) {
+			    int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			    switch (empCheck) {
 			   case IS_PART_TIME:
-			      empHrs= 4;
-			       break;
-			   case IS_FULL_TIME:
-			      empHrs = 8;
-			      break;
+			     empHrs = 4;
+			     break;
+			    case IS_FULL_TIME:
+			     empHrs = 8;
+			     break;
 			  default:
-			    empHrs = 0;
+			     empHrs = 0;
 			}
 			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
 			System.out.println("Emp Wage: " + empWage);
+			}
+			System.out.println("total Emp Wage: " + totalEmpWage);
 			    }
 			}
+
 
