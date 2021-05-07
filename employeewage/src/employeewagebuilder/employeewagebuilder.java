9392b5304a77511da6472d47ee0f3ce79466273f
pavan sriram prasad <pittapavansriramprasad@gmailcom>
2021-05-07 10:16:24 +0530

    First commit

---
+++ employeewage/src/employeewagebuilder/employeewagebuilder.java
@@ -0,0 +1,10 @@
+package employeewagebuilder;
+
+public class employeewagebuilder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ employeewage/src/module-info.java
@@ -0,0 +1,2 @@
+module employeewage {
+}
