0e33ab194d4924616cf0a2877da6d21b648dca08
pavan sriram prasad <pittapavansriramprasad@gmailcom>
2021-05-07 10:42:26 +0530

    Using Case Statement

--- employeewage/src/employeewagebuilder/employeewagebuilder.java
+++ employeewage/src/employeewagebuilder/employeewagebuilder.java
@@ -1,25 +1,29 @@
 package employeewagebuilder;
 
 public class employeewagebuilder {
-
+	 public static final int IS_PART_TIME = 1;
+	   public static final int IS_FULL_TIME = 2;
+	   public static final int EMP_RATE_PER_HOUR = 20;
+	   
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			   //constants
-			   int IS_PART_TIME = 1;
-			   int IS_FULL_TIME = 2;
-			   int EMP_RATE_PER_HOUR = 20;
-			   // variables
+			   //variables
 			   int empHrs = 0;
 			   int empWage = 0;
 			   // computation
-			  double empCheck = Math.floor(Math.random() * 10) % 3;
-			  if (empCheck == IS_PART_TIME)
-			      empHrs = 4;
-			  else if (empCheck == IS_FULL_TIME)
+			  int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			  switch (empCheck) {
+			   case IS_PART_TIME:
+			      empHrs= 4;
+			       break;
+			   case IS_FULL_TIME:
 			      empHrs = 8;
-			  else
-			      empHrs = 0;
-			   empWage = empHrs * EMP_RATE_PER_HOUR;
-			   System.out.println("Emp Wage: " + empWage);
-			   }
+			      break;
+			  default:
+			    empHrs = 0;
 			}
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			System.out.println("Emp Wage: " + empWage);
+			    }
+			}
+
