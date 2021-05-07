82f1465ae0d3fb7d38e417a9059853d0a693d16a
pavan sriram prasad <pittapavansriramprasad@gmailcom>
2021-05-07 10:30:42 +0530

    compute EmployeeWage

--- employeewage/src/employeewagebuilder/employeewagebuilder.java
+++ employeewage/src/employeewagebuilder/employeewagebuilder.java
@@ -4,13 +4,20 @@ public class employeewagebuilder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			       // constants
-			       int IS_FULL_TIME = 1;
-			       // computation
-			       double empCheck = Math.floor(Math.random() * 10) % 2;
-			       if (empCheck == IS_FULL_TIME)
-			       System.out.println("Employe is Present");
-			      else
-			       System.out.println("Employe is Absent");
+			    //constants
+			    int IS_FULL_TIME = 1;
+			    int EMP_RATE_PER_HOUR =20;
+			    //variables
+			    int empHrs = 0;
+			    int empWage = 0;
+
+			    //computation
+			  double empCheck = Math.floor(Math.random() *10) % 2;
+			  if (empCheck == IS_FULL_TIME)
+			      empHrs = 8;
+			else
+			    empHrs = 0;
+			  empWage = empHrs * EMP_RATE_PER_HOUR;
+			 System.out.println("Emp Wage: " + empWage);
 			    }
 }
