9c5ff5f76730bc066d414a68efa55f571643172f
pavanprasad222 <82253376+pavanprasad222@users.noreply.github.com>
2021-05-08 16:18:28 +0530

    Update employeewagebuilder.java

--- employeewage/src/employeewagebuilder/employeewagebuilder.java
+++ employeewage/src/employeewagebuilder/employeewagebuilder.java
@@ -4,6 +4,7 @@ public class employeewagebuilder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Welcome To EmployeeWage Computational Program");
 
 	}
 
