41349764696216f980acc2e193a59e5848f744d5
pavan sriram prasad <pittapavansriramprasad@gmailcom>
2021-05-10 07:45:29 +0530

    EmployeeWage for Multiple Company

--- employeewage/src/employeewagebuilder/employeewagebuilder.java
+++ employeewage/src/employeewagebuilder/employeewagebuilder.java
@@ -3,16 +3,14 @@ package employeewagebuilder;
 public class employeewagebuilder {
 	 public static final int IS_PART_TIME = 1;
 	   public static final int IS_FULL_TIME = 2;
-	   public static final int EMP_RATE_PER_HOUR = 20;
-	   public static final int  NO_OF_WORKING_DAYS = 2;
-	   public static final int MAX_HRS_IN_MONTH = 10;
 	   
-	public static int computeEmpWage() {
+	public static int computeEmpWage(String company, int EMP_RATE_PER_HOUR,
+			                            int NO_OF_WORKING_DAYS, int MAX_HOUR_PER_MONTH) {
 		// TODO Auto-generated method stub
 			  //variables
 			  int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 			  //computation
-			  while (totalEmpHrs <= MAX_HRS_IN_MONTH  &&
+			  while (totalEmpHrs <= MAX_HOUR_PER_MONTH  &&
 			          totalWorkingDays < NO_OF_WORKING_DAYS) {
 			   totalWorkingDays++;
 			  int empCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -30,13 +28,14 @@ public class employeewagebuilder {
 			System.out.println("Day#: " + totalWorkingDays + " Emp hr: " +empHrs);
 			}
 			int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-			System.out.println("Total Emp Wage: " + totalEmpWage);
+			System.out.println("Total Emp Wage for Company: " +company+" is: "+ totalEmpWage);
 			
 			return totalEmpWage;
 	}
 	
 	public static void main(String[] args) {
-		computeEmpWage();
+		computeEmpWage("SRMT", 30, 20, 50);
+		computeEmpWage("Dmart", 20, 10, 40);
 			   }
 	}
 
